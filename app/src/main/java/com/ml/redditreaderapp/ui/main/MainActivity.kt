84bb4d1e2a6119339bf18945fdb5165adf61ebf0
Matthew Levene <matthew.levene@hotmail.co.uk>
2021-03-09 09:46:32 +0000

    Add initial project files

---
+++ app/src/main/java/com/ml/redditreaderapp/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ml.redditreaderapp.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ml.redditreaderapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
